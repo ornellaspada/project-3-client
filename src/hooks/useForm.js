01495a8d61b20a946d4ec2c540028a18c2d477f9
“gursham” <Gursham2001@hotmail.com>
2021-05-26 12:34:29 +0100

    form commit

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function useForm(intialState) {
-  const [formData, setFormData] = React.useState(intialState)
-  const [formErrors, setFormErrors] = React.useState(intialState)
+export function useForm(initialState) {
+  const [formData, setFormData] = React.useState(initialState)
+  const [formErrors, setFormErrors] = React.useState(initialState)
 
   const handleChange = event => {
     setFormData({ ...formData, [event.target.name]: event.target.value })
